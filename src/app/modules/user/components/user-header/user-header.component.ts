17b1f573aaa4771d76f0136183f8e28c8e1af2fb
Fabricio Murillo Flores <Fabriciomf72@gmail.com>
2022-06-08 19:44:10 -0600

    [ADD] user menu styles

--- src/app/modules/user/components/user-header/user-header.component.ts
+++ src/app/modules/user/components/user-header/user-header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ADD_USER_BUTTON_TEXT, ADD_USER_ROUTE, USER_TITLE_TEXT } from 'src/app/modules/core/constants/user.constant';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/user';
+import { ADD_USER_BUTTON_TEXT, ADD_USER_ROUTE, ALL_STATUS, USER_TITLE_TEXT } from 'src/app/modules/core/constants/user.constant';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-header',
@@ -7,14 +10,30 @@ import { ADD_USER_BUTTON_TEXT, ADD_USER_ROUTE, USER_TITLE_TEXT } from 'src/app/m
   styleUrls: ['./user-header.component.css']
 })
 export class UserHeaderComponent implements OnInit {
-  
-  userListLength:number = 0;
-  userTitleText:string = USER_TITLE_TEXT; 
-  addUserButtonText:string = ADD_USER_BUTTON_TEXT;
-  addUserRoute:string = ADD_USER_ROUTE;
-  constructor() { }
+
+  userListLength: number = 0;
+  userTitleText: string = USER_TITLE_TEXT;
+  addUserButtonText: string = ADD_USER_BUTTON_TEXT;
+  addUserRoute: string = ADD_USER_ROUTE;
+  userList!: User[];
+  userSubscription$!: Subscription;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    const userObservable$ = this.userService.getUserList();
+    this.userSubscription$ = userObservable$.subscribe((userList: User[]) => {
+      this.userList = userList
+      this.userListLength = userList.length;
+    }
+    );
+    this.userService.filterUsersByStatus(ALL_STATUS);
+    console.log(this.userList)
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroy');
+    this.userSubscription$.unsubscribe();
   }
 
 }
